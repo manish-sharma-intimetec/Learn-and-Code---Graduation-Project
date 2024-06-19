223b3641df2f7ce4651a55787a860b30c3810170
manish-sharma-intimetec <manish.sharma@intimetec.com>
2024-06-19 14:03:31 +0530

    adding some new files and modified some files.

---
+++ Project/Server/ProtocolDataUnit.py
@@ -0,0 +1,7 @@
+class ProtocolDataUnit:
+    def __init__(self) -> None:
+        self.PDU = {
+    "payload": None,
+    "status": None,
+    "time": None
+}
--- Project/Server/Server.py
+++ Project/Server/Server.py
@@ -1,28 +1,60 @@
 import threading
 import socket
+from ProtocolDataUnit import ProtocolDataUnit
+import json
+import ast
+import queue
 
 class Server:
     def __init__(self, HOST: str, PORT: int) -> None:
         self.HOST = HOST
         self.PORT = PORT
+        self.outputQueue = queue.Queue()
     
+    def convertProtocolDataUnitIntoDictionary(self, data: str):
+        data = ast.literal_eval(data)
+        return data
+    
+    def isPDUValid(self, PDU: dict) -> bool:
+        isValid = True
+
+        if(PDU['status'] != 'OK'):
+            isValid = False
+        if(PDU['payload'] == None or len(PDU['payload']) <= 0):
+            isValid = False
+        if(PDU['time'] == None):
+            isValid = False
+        
+        return isValid
+
+    def sendResponse(self, connection, clientData: dict, responseMessage: str):
+        if self.isPDUValid(clientData) == True:
+            connection.send(responseMessage.encode('utf-8'))
+        else:
+            print("PDU have some issue.")
+
+
     def createSocket(self, HOST: str, PORT: int) -> socket.socket:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((HOST, PORT))
         return server
     
-    def listenClient(self, server: socket.socket):
+    def listenClient(self, server: socket.socket) -> str:
         connection, clientAddress = server.accept()  
-        # print(connection)
         data = connection.recv(1024).decode("UTF-8")
-        print(data)
+        # print(data)
+        PDU = self.convertProtocolDataUnitIntoDictionary(data)
+        print(PDU)
+        self.sendResponse(connection, PDU, "Got it.")
+        # connection.send("Hello".encode('utf-8'))
+        return data
     
     def listenMultipleClients(self):
         server = self.createSocket(self.HOST, self.PORT)
         server.listen()
         
         while True:
-            thread = threading.Thread(target=self.listenClient, args=(server,))
+            thread = threading.Thread(target=self.listenClient, args=(server, ))
             thread.start()
 
 
